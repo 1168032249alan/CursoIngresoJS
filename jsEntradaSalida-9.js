609018a0129749ee3ee6b11f64ceebc5126d302c
1168032249alan <36265104+1168032249alan@users.noreply.github.com>
2018-02-09 12:27:49 -0300

    ejercicios 7, 8 y 9
    resto, aumento ,suma ...

---
+++ jsEntradaSalida-9.js
@@ -0,0 +1,20 @@
+/*Debemos lograr tomar el importe por ID ,
+transformarlo a entero (parseInt), luego
+mostrar el importe con un aumento del 10 %
+en el cuadro de texto "RESULTADO".*/
+function MostrarAumento()
+ {  
+     var sueldo;
+    var aumento;
+    var RESULTADO;
+
+    sueldo=document.getElementById("sueldo").value;
+ 
+
+    sueldo=parseInt(sueldo);
+
+    resultado=sueldo*1.1;
+
+    document.getElementById("resultado").value=resultado;
+	
+}
